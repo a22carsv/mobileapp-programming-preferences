123ac1f03caf71a4bd583cba9e4b27eb3ad46654
Zepronex <callesvensson@hotmail.se>
2023-05-18 00:28:50 +0200

    Modified MainActivity class to read data from Shared Preferences, created a new screen called SecondActivity that can be opened from MainActivity.

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.project;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -7,12 +8,27 @@ import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    private SharedPreferences sharedPreferences;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Read data from Shared Preferences
+        String data = sharedPreferences.getString("data", "");
+        // Use the retrieved data as needed in MainActivity
+        // For example, you can update a TextView with the data:
+        // TextView textView = findViewById(R.id.textView);
+        // textView.setText(data);
+    }
 }
